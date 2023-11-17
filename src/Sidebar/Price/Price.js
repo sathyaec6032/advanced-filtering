d31f4cbc1afa3509f523a27901f8c4246c3796f1
sathyaec6032 <sathyaec6032@gmail.com>
2023-11-17 17:30:46 +0530

    first commit

---
+++ src/Sidebar/Price/Price.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Price.css";
+import Input from "../../components/Input";
+
+function Price({ handleChange }) {
+  return (
+    <div className="ml">
+      <h2 className="sidebar-title price-title">Price</h2>
+      <Input handleChange={handleChange} value="" title="All" name="test2" />
+      <Input
+        handleChange={handleChange}
+        value={100}
+        title="$50 - $100"
+        name="test2"
+      />
+      <Input
+        handleChange={handleChange}
+        value={150}
+        title="$100 - $150"
+        name="test2"
+      />
+      <Input
+        handleChange={handleChange}
+        value={200}
+        title="Over $150"
+        name="test2"
+      />
+    </div>
+  );
+}
+
+export default Price;
